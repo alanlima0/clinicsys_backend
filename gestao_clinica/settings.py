dc9c4005dd6a37e31bff1c1897790be41d408153
alanlima0 <alanlima05022003@gmail.com>
2026-01-27 19:25:27 -0300

    Implementação de logout

--- gestao_clinica/settings.py
+++ gestao_clinica/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'pacientes',
     'users',
 
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+
 ]
 
 MIDDLEWARE = [
@@ -153,6 +156,10 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+
     'AUTH_HEADER_TYPES': ('Bearer',),
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
 }
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
-# users/views.py
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .models import User
 from .serializers import (
     UserSerializer,
@@ -10,7 +14,7 @@ from .serializers import (
 from .permissions import IsAdminClinica
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     permission_classes = [IsAuthenticated, IsAdminClinica]
 
     def get_serializer_class(self):
@@ -19,3 +23,34 @@ class UserViewSet(ModelViewSet):
         elif self.action in ['update', 'partial_update']:
             return UserUpdateSerializer
         return UserSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        if request.user.id == self.get_object().id:
+            return Response(
+                {"detail": "Você não pode excluir seu próprio usuário"},
+                status=400
+            )
+        return super().destroy(request, *args, **kwargs)
+    
+
+      # 🔐 LOGOUT
+    @action(
+        detail=False,
+        methods=['post'],
+        permission_classes=[IsAuthenticated]
+    )
+    def logout(self, request):
+        try:
+            refresh_token = request.data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(
+                {"detail": "Logout realizado com sucesso"},
+                status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception:
+            return Response(
+                {"detail": "Refresh token inválido ou ausente"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
