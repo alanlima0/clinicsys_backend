02041d2696b032ce99b573ede4e4887d9375dbfc
alanlima0 <alanlima05022003@gmail.com>
2026-03-07 16:46:08 -0300

    adicao do atributo glicemia no model triagem

--- atendimentos/models.py
+++ atendimentos/models.py
@@ -42,6 +42,7 @@ class Triagem(models.Model):
     temperatura = models.DecimalField(max_digits=4, decimal_places=1)
     saturacao = models.PositiveSmallIntegerField()
     frequencia_cardiaca = models.PositiveSmallIntegerField()
+    glicemia = models.PositiveSmallIntegerField(null=True, blank=True)
     
     criado_em = models.DateTimeField(auto_now_add=True)
 
